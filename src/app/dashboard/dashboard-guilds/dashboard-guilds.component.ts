3989c2236140ec0f3fa5bfc6a6ea437f1e999492
Sam512 <33881069+Sam512@users.noreply.github.com>
2019-06-24 20:46:02 +0100

    New Support Links

--- src/app/dashboard/dashboard-guilds/dashboard-guilds.component.ts
+++ src/app/dashboard/dashboard-guilds/dashboard-guilds.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-dashboard-guilds',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard-guilds.component.css']
 })
 export class DashboardGuildsComponent implements OnInit {
-
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
+    this.http
+      .get('https://api.campfirebot.xyz/users/me/guilds', {
+        headers: {
+          Authorization: localStorage.getItem('auth')
+        }
+      })
+      .subscribe(data => {
+        console.log(data);
+      });
   }
-
 }
