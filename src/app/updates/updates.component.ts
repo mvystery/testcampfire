e55e4ef16296d275f9e23ebfe2fbc60d56c33161
Sam512 <33881069+Sam512@users.noreply.github.com>
2019-08-28 10:31:31 +0100

    Removed Circular

--- src/app/updates/updates.component.ts
+++ src/app/updates/updates.component.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./updates.component.css']
 })
 export class UpdatesComponent implements OnInit {
+  open = false;
+  response: string;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  openModal() {
+    this.open = true;
   }
 
+  closeModal() {
+    this.open = false;
+  }
+
+  feedback(rev) {
+    if (rev === 'yes') {
+      this.response = 'Thank you!';
+    } else if (rev === 'meh') {
+      this.response = 'Thank you!';
+    } else {
+      // tslint:disable-next-line: quotemark
+      this.response = "We're sorry!";
+    }
+  }
 }
