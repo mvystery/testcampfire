34fd3e61b6ad7430b8cc28864b005f35cfcbaf48
Sam512 <33881069+Sam512@users.noreply.github.com>
2019-10-30 16:37:47 +0000

    Verification

--- src/app/verify/verify.component.ts
+++ src/app/verify/verify.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import ClipboardJS from 'clipboard';
 
 @Component({
   selector: 'app-verify',
@@ -9,39 +11,92 @@ export class VerifyComponent implements OnInit {
   step = 1;
 
   robloxUsername: string;
-
+  readyForRoblox: boolean;
+  error: boolean;
+  loadingSubmit: boolean;
   code: string;
 
-  constructor() {}
+  finalStepError: string;
+
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
-    const list = [
-      'dog ',
-      'cat ',
-      'campfire ',
-      'chicken ',
-      'fish ',
-      'meow ',
-      'roblox ',
-      'epic ',
-      'friend ',
-      'hello ',
-      'hi ',
-      'amazing ',
-      'cool '
-    ];
+    const clipboard = new ClipboardJS('.clip-button');
 
-    const result =
-      list[Math.floor(Math.random() * list.length)] +
-      list[Math.floor(Math.random() * list.length)] +
-      list[Math.floor(Math.random() * list.length)] +
-      list[Math.floor(Math.random() * list.length)] +
-      list[Math.floor(Math.random() * list.length)];
-    this.code = result.slice(0, -1);
+    clipboard.on('success', e => {
+      e.trigger.classList.add('is-success');
+      e.trigger.innerHTML = '<i class="far fa-clipboard"></i> Copied';
+    });
+
+    this.http
+      .get<any>('https://api.campfirebot.xyz/verify/create', {
+        headers: {
+          Authorization: localStorage.getItem('auth')
+        }
+      })
+      .subscribe(data => {
+        if (data.ready === true) {
+          this.readyForRoblox = true;
+          console.log(data.code);
+          this.code = data.code;
+        } else {
+          this.readyForRoblox = false;
+          this.error = true;
+        }
+      });
   }
 
   usernameForm(data) {
     this.robloxUsername = data.value.username;
     this.step = 2;
+  }
+
+  forceVerify() {
+    this.step = 1;
+    this.error = false;
+    this.http
+      .get<any>('https://api.campfirebot.xyz/verify/create/force', {
+        headers: {
+          Authorization: localStorage.getItem('auth')
+        }
+      })
+      .subscribe(data => {
+        if (data.ready === true) {
+          this.readyForRoblox = true;
+          console.log(data.code);
+          this.code = data.code;
+        } else {
+          this.readyForRoblox = false;
+          this.error = true;
+        }
+      });
+  }
+
+  verifyCodeDone() {
+    this.loadingSubmit = true;
+    this.http
+      .get<any>(
+        `https://api.campfirebot.xyz/verify/complete/${this.robloxUsername}`,
+        {
+          headers: {
+            Authorization: localStorage.getItem('auth')
+          }
+        }
+      )
+      .subscribe(data => {
+        if (data.success) {
+          this.loadingSubmit = false;
+          this.step = 3;
+        } else {
+          this.loadingSubmit = false;
+          this.step = 4;
+          this.finalStepError = data.reason;
+        }
+      });
+  }
+
+  goBackToStart() {
+    this.step = 1;
+    this.robloxUsername = '';
   }
 }
