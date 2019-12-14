d7dc2989951c6b4291fe57733e6a0398658fd790
Sam512 <33881069+Sam512@users.noreply.github.com>
2019-12-14 17:34:10 +0000

    Fixed LoadingData

--- src/app/dashboard/settings/logging/logging.component.ts
+++ src/app/dashboard/settings/logging/logging.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-logging',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./logging.component.css']
 })
 export class LoggingComponent implements OnInit {
+  @Input() id: number;
+  loadingData = true;
 
   constructor() { }
 
--- src/app/dashboard/settings/moderator/moderator.component.ts
+++ src/app/dashboard/settings/moderator/moderator.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-moderator',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./moderator.component.css']
 })
 export class ModeratorComponent implements OnInit {
+  @Input() id: number;
+  loadingData = true;
 
   constructor() { }
 
