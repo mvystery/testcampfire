32b453d26f7d2d59100c46582ce499bca9fd8f96
Sam512 <33881069+Sam512@users.noreply.github.com>
2020-01-16 20:35:43 +0000

    Fixed clover bug

--- src/app/clover/clover-user/clover-unav/clover-unav.component.ts
+++ src/app/clover/clover-user/clover-unav/clover-unav.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./clover-unav.component.css']
 })
 export class CloverUnavComponent implements OnInit {
+  fixed: boolean;
 
   constructor() { }
 
